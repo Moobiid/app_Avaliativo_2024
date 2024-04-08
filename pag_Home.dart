d040bc406d4dafc936af84fef15006aa6a085ab3
João Victor Mobiglia Podenciano <111130621+Moobiid@users.noreply.github.com>
2024-04-08 14:31:06 -0300

    Create pag_Home.dart

---
+++ pag_Home.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class PagHome extends StatelessWidget {
+  const PagHome({super.key}); //construtor da classe
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+        body: Center(
+      child: Text('Tebm-vindo a tela inicial!'),
+    ));
+  }
+}
