d0d9e15768a89015fa674e67e55cb2d476b90883
João Victor Mobiglia Podenciano <111130621+Moobiid@users.noreply.github.com>
2024-04-08 14:30:45 -0300

    Create pag_Explorar.dart

---
+++ pag_Explorar.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart ';
+
+import 'pag_Home.dart';
+
+class PagExplorar extends StatelessWidget {
+  const PagExplorar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Center(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 25.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            //logotipo do app
+
+            Padding(
+              padding: const EdgeInsets.all(25.0),
+              child: Center(
+                child: Image.asset(
+                  'lib/imagens/logo4.png',
+                ),
+              ),
+            ),
+
+            const SizedBox(height: 2),
+
+            //botão de explorar
+
+            ElevatedButton(
+              onPressed: () => Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => const PagHome())),
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.black,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                padding: const EdgeInsets.all(25),
+              ),
+              child: const Text(
+                'Explorar',
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold),
+              ),
+            ),
+          ],
+        ),
+      ),
+    ));
+  }
+}
