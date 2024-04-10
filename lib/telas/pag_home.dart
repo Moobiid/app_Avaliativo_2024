9459e2a1d40b5a43a5bfb0113f141b529bc49a77
João Victor Mobiglia Podenciano <111130621+Moobiid@users.noreply.github.com>
2024-04-10 02:52:34 -0300

    Update pag_home.dart
    Alterado o Widget para Stateful
    
    Criado um sistema de navegação usando uma Lista

--- lib/telas/pag_home.dart
+++ lib/telas/pag_home.dart
@@ -1,45 +1,54 @@
-import 'package:app_avaliativo/telas/pag_intro.dart';
+import 'package:app_avaliativo/componentes/barra_navegacao.dart';
+//import 'package:app_avaliativo/telas/pag_intro.dart';
 import 'package:flutter/material.dart';
+
+import 'pag_carrinho.dart';
+import 'pag_compras.dart';
+import 'pag_favoritos.dart';
+import 'pag_pesquisa.dart';
 //import 'package:device_preview/device_preview.dart';
 
-class PagHome extends StatelessWidget {
+class PagHome extends StatefulWidget {
   const PagHome({super.key}); //construtor da classe
+
+  @override
+  State<PagHome> createState() => _PagHomeState();
+}
+
+class _PagHomeState extends State<PagHome> {
+//criando um indicador para ter melhor controle sobre a barra de navegacao
+  int _indexAtual = 0;
+
+//criadno um metodo para navegar entre as telas
+//quando o usuario clicar em um item da barra de navegacao
+  void navigateBottomBar(int index) {
+    setState(() {
+      _indexAtual = index;
+    });
+  }
+
+  //criando uma lista de widgets para armazenar as telas
+  final List<Widget> _telas = [
+    //Pagina de compras
+    const PagCompras(),
+
+    //Pagina de pesquisa
+    const PagPesquisa(),
+
+    //Pagina de favoritos
+    const PagFavoritos(),
+
+    //Pagina de carrinho
+    const PagCarrinho(),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: [
-          // Resto do seu layout
-          const Center(
-            child: Padding(
-              padding: EdgeInsets.symmetric(horizontal: 25.0),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  //Criando um texto para exibição na tela
-                  Text('CARAIO'),
-                ],
-              ),
-            ),
-          ),
-          // Botão de voltar
-          Positioned(
-            top: 40.0, // ajuste conforme necessário
-            left: 10.0, // ajuste conforme necessário
-            child: IconButton(
-              icon: const Icon(
-                Icons.arrow_back_ios_rounded,
-                color: Colors.black,
-              ),
-              onPressed: () => Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => const PagExplorar()),
-              ),
-            ),
-          ),
-        ],
+      bottomNavigationBar: BarraNavegacao(
+        onTabChange: (index) => navigateBottomBar(index),
       ),
+      body: _telas[_indexAtual],
     );
   }
 }
